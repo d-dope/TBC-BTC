412fc5d80cf1edf3a054d892d0111105acccf118
Davit <davitsulkhanishvili18@gmail.com>
2024-01-26 04:07:25 +0400

    made structure stilying has left

--- main.js
+++ main.js
@@ -6,4 +6,29 @@ hamburger.addEventListener('click', () => {
 });
 
 
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+
+    // Rotate the SVG when answer is opened
+    const svg = faq.querySelector('svg');
+    if (faq.classList.contains("active")) {
+      svg.style.transform = "rotate(180deg)";
+    } else {
+      svg.style.transform = "rotate(0deg)";
+    }
+
+    // Close other opened questions
+    faqs.forEach(otherFaq => {
+      if (otherFaq !== faq && otherFaq.classList.contains("active")) {
+        otherFaq.classList.remove("active");
+        // Rotate the SVG back to its original position
+        const otherSvg = otherFaq.querySelector('svg');
+        otherSvg.style.transform = "rotate(0deg)";
+      }
+    });
+  });
+});
 
